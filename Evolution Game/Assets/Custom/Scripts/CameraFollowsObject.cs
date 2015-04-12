443f2bf6a8fccfc55a6ba82f9851145505902f3f
Christopher Berg <cb@wps.de>
2015-04-12 21:01:57 +0200

    first fish with basic movement and camera setup

---
+++ Evolution Game/Assets/Custom/Scripts/CameraFollowsObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowsObject : MonoBehaviour {
+
+	public GameObject target;
+	public float cameraVelocity = 0.0F;
+	public float cameraSmoothTime = 0.1F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newXPosition = Mathf.SmoothDamp (this.transform.position.x, target.transform.position.x, ref cameraVelocity, cameraSmoothTime);
+		this.transform.position = new Vector3 (newXPosition, this.transform.position.y, this.transform.position.z);
+	}
+}
---
+++ Evolution Game/Assets/Custom/Scripts/FishController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishController : MonoBehaviour {
+
+	private CharacterController controller;
+	private Vector3 speed = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speed.x = Input.GetAxis ("Horizontal");
+
+		controller.Move(speed);
+	}
+}
