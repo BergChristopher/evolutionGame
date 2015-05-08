7ddc6f02bc0ddbe531b13f78e6d36cca4be3a43c
Christopher Berg <cb@wps.de>
2015-05-08 22:40:16 +0200

    added plant script fish can destroy plants by touching them

---
+++ Evolution Game/Assets/Custom/Scripts/Plant.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plant : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D enteringCollider) {
+		Debug.Log ("trigger enter");
+		if (enteringCollider.gameObject.tag == "Player") {
+			Debug.Log ("is player");
+			Destroy(this.gameObject);
+		}
+	}
+
+}
